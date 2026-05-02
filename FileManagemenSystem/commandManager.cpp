96e0be6d0872ea1258a1fa7896af279efc73d403
Muflehh <muflehahmad4206@gmail.com>
2026-05-02 18:27:24 +0500

    Update commandManager.cpp
    I added my else if conditions for "audio" and "Private" file

--- FileManagemenSystem/commandManager.cpp
+++ FileManagemenSystem/commandManager.cpp
@@ -3,24 +3,46 @@
 #include"Node.h"
 #include"Folder.h"
 #include"text.h"
+#include"mp3.h"
+#include "privateFile.h"
 
 Commands::Commands() {
-    rootFolder = new Folder("root", nullptr);
-    currentFolder = rootFolder;
+	rootFolder = new Folder("root", nullptr);
+	currentFolder = rootFolder;
 }
 void Commands::executeMkdir(std::string dirName) {
 	Node* newFolder = new Folder(dirName, currentFolder);
 	currentFolder->addNode(newFolder);
 }
 
-void Commands::executeTouch(std::string textName) {
-
-	Node* newFile = new textFile(textName, currentFolder);
-	currentFolder->addNode(newFile);
+void Commands::executeTouch(std::string fileType,std::string textName) {
+	if (fileType=="txt")
+	{
+		Node* newFile = new textFile(textName, currentFolder);
+		currentFolder->addNode(newFile);
+	}
+	else if (fileType == "audio") {
+		Node* newFile = new audioFile(textName, currentFolder);
+		currentFolder->addNode(newFile);
+		std::cout << "Audio File is Created bro: " << textName << "\n";
+	}
+	else if (fileType == "private" || "Private")
+	{
+		std::string passkey;
+		std::cout << "Enter passkey for this file: ";
+		std::getline(std::cin, passkey);
+		Node* newFile = new privateFile(textName, currentFolder, passkey);
+		currentFolder->addNode(newFile);
+		std::cout << "Youre new Private File has been Created!" << "\n";
+	}
+	else {
+		std::cout << "Unknown file type: " << fileType << "\n";
+		std::cout << "Valid types are: txt, audio, private only !\n";
+	}
 }
 
 void Commands::executeRename(std::string oldName, std::string newName) {
-	currentFolder->renameNode(oldName,newName);
+	currentFolder->renameNode(oldName, newName);
 }
 
 void Commands::executeLs() {
@@ -28,7 +50,7 @@ void Commands::executeLs() {
 }
 
 void Commands::executeCd(std::string NodeName) {
-	
+
 	// root> cd folder will bring user inside folder which means the only chang will be currentFolder
 	if (NodeName == "..") {
 		currentFolder = rootFolder;
@@ -42,12 +64,12 @@ void Commands::executeCd(std::string NodeName) {
 			currentFolder = (Folder*)currentFolder->findChild(NodeName);
 		}
 		else {
-			std:: cout << "There is no directory names as " << NodeName << " Instead its a file " << std::endl;
+			std::cout << "There is no directory names as " << NodeName << " Instead its a file " << std::endl;
 		}
 
 	}
 	else {
-		std :: cout << "Error: " << NodeName << " directory not found!" << std :: endl;
+		std::cout << "Error: " << NodeName << " directory not found!" << std::endl;
 	}
 
 
